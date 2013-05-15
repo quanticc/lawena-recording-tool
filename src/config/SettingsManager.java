8c5da123c21991945b2a6051733bc8b0f8f7d577
Ivan Abarca <ivan.abarca@usach.cl>
2013-05-15 06:02:00 -0400

    better settings management

--- src/config/SettingsManager.java
+++ src/config/SettingsManager.java
@@ -1,159 +1,73 @@
 
 package config;
 
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Properties;
 import java.util.logging.Logger;
 
 public class SettingsManager {
 
     private static final Logger log = Logger.getLogger("lwrt");
 
+    private enum Key {
+        Height(720), Width(1280), Framerate(120), ViewmodelFov(70), DxLevel(98), Hud("medic"), ViewmodelSwitch(
+                "on"), MotionBlur(true), CrosshairSwitch(false), Crosshair(false), CombatText(false), Announcer(
+                true), Domination(true), Hitsounds(false), Voice(true), SteamCloud(false);
+
+        private Object value;
+
+        private Key(Object value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return name().toLowerCase();
+        }
+    }
+
     private String filename;
-    private int height = 720;
-    private int width = 1280;
-    private int framerate = 120;
-    private int viewmodelfov = 70;
-    private int dxlevel = 98;
-    private String hud = "medic";
-    private String viewmodelswitch = "on";
-    private boolean motionblur = true;
-    private boolean crosshairswitch = false;
-    private boolean crosshair = false;
-    private boolean combattext = false;
-    private boolean announcer = true;
-    private boolean domination = true;
-    private boolean hitsounds = false;
-    private boolean voice = true;
-    private boolean steamcloud = false;
+    private Properties properties;
 
     public SettingsManager(String settingsFile) {
         filename = settingsFile;
+        Properties defaults = new Properties();
+        for (Key key : Key.values()) {
+            defaults.setProperty(key.toString(), key.value + "");
+        }
+        properties = new Properties(defaults);
         try {
-            BufferedReader settings = new BufferedReader(new FileReader(filename));
-            String line;
-            while ((line = settings.readLine()) != null) {
-                if (line.charAt(0) == ':') {
-                    continue;
-                }
-                if (line.indexOf("height") >= 0) {
-                    try {
-                        height = Integer.parseInt(line.substring(line.indexOf('=') + 1));
-                    } catch (NumberFormatException e) {
-                        log.info("Bad numeric format: height");
-                    }
-                }
-                if (line.indexOf("width") >= 0) {
-                    try {
-                        width = Integer.parseInt(line.substring(line.indexOf('=') + 1));
-                    } catch (NumberFormatException e) {
-                        log.info("Bad numeric format: width");
-                    }
-                }
-                if (line.indexOf("frame rate") >= 0) {
-                    try {
-                        framerate = Integer.parseInt(line.substring(line.indexOf('=') + 1));
-                    } catch (NumberFormatException e) {
-                        log.info("Bad numeric format: framerate");
-                    }
-                }
-                if (line.indexOf("hud") >= 0) {
-                    hud = line.substring(line.indexOf('=') + 1);
-                }
-                if (line.indexOf("viewmodel switching") >= 0) {
-                    viewmodelswitch = line.substring(line.indexOf('=') + 1);
-                }
-                if (line.indexOf("motion") >= 0) {
-                    motionblur = line.substring(line.indexOf('=') + 1).equals("on") ? true : false;
-                }
-                if (line.indexOf("viewmodel fov") >= 0) {
-                    try {
-                        viewmodelfov = Integer.parseInt(line.substring(line.indexOf('=') + 1));
-                    } catch (NumberFormatException e) {
-                        log.info("Bad numeric format: viewmodel fov");
-                    }
-                }
-                if (line.indexOf("dxlevel") >= 0) {
-                    try {
-                        dxlevel = Integer.parseInt(line.substring(line.indexOf('=') + 1));
-                    } catch (NumberFormatException e) {
-                        log.info("Bad numeric format: dxlevel");
-                    }
-                }
-                if (line.indexOf("crosshair switching") >= 0) {
-                    crosshairswitch = line.substring(line.indexOf('=') + 1).equals("on") ? true
-                            : false;
-                }
-                if (line.indexOf("crosshair") >= 0) {
-                    crosshair = line.substring(line.indexOf('=') + 1).equals("on") ? true : false;
-                }
-                if (line.indexOf("combat text") >= 0) {
-                    combattext = line.substring(line.indexOf('=') + 1).equals("on") ? true : false;
-                }
-                if (line.indexOf("announcer") >= 0) {
-                    announcer = line.substring(line.indexOf('=') + 1).equals("on") ? true : false;
-                }
-                if (line.indexOf("domination") >= 0) {
-                    domination = line.substring(line.indexOf('=') + 1).equals("on") ? true : false;
-                }
-                if (line.indexOf("hit sounds") >= 0) {
-                    hitsounds = line.substring(line.indexOf('=') + 1).equals("on") ? true : false;
-                }
-                if (line.indexOf("voice") >= 0) {
-                    voice = line.substring(line.indexOf('=') + 1).equals("on") ? true : false;
-                }
-                if (line.indexOf("cloud") >= 0) {
-                    steamcloud = line.substring(line.indexOf('=') + 1).equals("on") ? true : false;
-                }
-            }
-            settings.close();
+            properties.load(new FileReader(filename));
+            log.info("Settings loaded from " + filename);
         } catch (FileNotFoundException e) {
-            log.info("Settings file not found, loading default values");
+            log.info("Settings file not found, loading defaults");
         } catch (IOException e) {
-            log.info("Problem reading through settings file, will keep some default values");
+            log.info("Problem while loading settings, reverting to defaults");
         }
     }
 
     public void save() throws Exception {
-        PrintWriter settings = new PrintWriter(new FileWriter(filename));
-        settings.println(": Settings File, feel free to customize manually");
-        settings.println(":");
-        settings.println(": Resolution Settings");
-        settings.println("width=" + width);
-        settings.println("height=" + height);
-        settings.println(":");
-        settings.println(": Framerate Settings");
-        settings.println("frame rate=" + framerate);
-        settings.println(":");
-        settings.println(": HUD Settings");
-        settings.println(": No HUD = killnotices");
-        settings.println(": Medic HUD = medic");
-        settings.println(": Full HUD = full");
-        settings.println(": Custom HUD = custom");
-        settings.println("hud=" + hud);
-        settings.println(":");
-        settings.println(": Misc Settings");
-        settings.println("viewmodel fov=" + viewmodelfov);
-        settings.println("motion blur=" + (motionblur ? "on" : "off"));
-        settings.println("viewmodel switching=" + viewmodelswitch);
-        settings.println("crosshair switching=" + (crosshairswitch ? "on" : "off"));
-        settings.println("crosshair=" + (crosshair ? "on" : "off"));
-        settings.println("combat text=" + (combattext ? "on" : "off"));
-        settings.println("announcer voice=" + (announcer ? "on" : "off"));
-        settings.println("domination sounds=" + (domination ? "on" : "off"));
-        settings.println("hit sounds=" + (hitsounds ? "on" : "off"));
-        settings.println("voice=" + (voice ? "on" : "off"));
-        settings.println("steam cloud=" + (steamcloud ? "on" : "off"));
-        settings.println("dxlevel=" + dxlevel);
-        settings.close();
+        log.info("Storing config to " + filename);
+        PrintWriter pw = new PrintWriter(new FileWriter(filename));
+        properties.store(pw, "lawena settings");
     }
 
     public void saveToCfg() throws IOException {
         PrintWriter settings = new PrintWriter(new FileWriter("cfg\\settings.cfg"));
+        int framerate = getFramerate();
+        boolean motionblur = getMotionBlur();
+        boolean steamcloud = getSteamCloud();
+        int viewmodelfov = getViewmodelFov();
+        String viewmodelswitch = getViewmodelSwitch();
+        boolean crosshairswitch = getCrosshairSwitch();
+        boolean crosshair = getCrosshair();
+        boolean combattext = getCombattext();
+        boolean hitsounds = getHitsounds();
+        boolean voice = getVoice();
         settings.println("alias recframerate host_framerate " + framerate);
         if (framerate < 60) {
             settings.println("alias currentfpsup 60fps");
@@ -237,135 +151,164 @@ public class SettingsManager {
         settings.close();
     }
 
+    private void setString(Key key, String value) {
+        properties.setProperty(key.toString(), value);
+    }
+
+    private String getString(Key key) {
+        return properties.getProperty(key.toString());
+    }
+
+    private void setInt(Key key, int value) {
+        properties.setProperty(key.toString(), value + "");
+    }
+
+    private int getInt(Key key) {
+        try {
+            return Integer.parseInt(properties.getProperty(key.toString()));
+        } catch (NumberFormatException e) {
+            log.info("Invalid numeric format in key: " + key + ", loading default value");
+            return Integer.parseInt(key.value.toString());
+        }
+    }
+
+    private void setBoolean(Key key, boolean value) {
+        properties.setProperty(key.toString(), value + "");
+    }
+
+    private boolean getBoolean(Key key) {
+        return Boolean.parseBoolean(properties.getProperty(key.toString()));
+    }
+
     public void setFilename(String filename) {
         this.filename = filename;
     }
 
-    public void setHeight(int height) {
-        this.height = height;
+    public void setHeight(int value) {
+        setInt(Key.Height, value);
     }
 
-    public void setWidth(int width) {
-        this.width = width;
+    public void setWidth(int value) {
+        setInt(Key.Width, value);
     }
 
-    public void setFramerate(int framerate) {
-        this.framerate = framerate;
+    public void setFramerate(int value) {
+        setInt(Key.Framerate, value);
     }
 
-    public void setHud(String hud) {
-        this.hud = hud;
+    public void setHud(String value) {
+        setString(Key.Hud, value);
     }
 
-    public void setViewmodelFov(int viewmodelfov) {
-        this.viewmodelfov = viewmodelfov;
+    public void setViewmodelFov(int value) {
+        setInt(Key.ViewmodelFov, value);
     }
 
-    public void setMotionBlur(boolean motionblur) {
-        this.motionblur = motionblur;
+    public void setMotionBlur(boolean value) {
+        setBoolean(Key.MotionBlur, value);
     }
 
-    public void setViewmodelSwitch(String viewmodelswitch) {
-        this.viewmodelswitch = viewmodelswitch;
+    public void setViewmodelSwitch(String value) {
+        setString(Key.ViewmodelSwitch, value);
     }
 
-    public void setCrosshairSwitch(boolean crosshairswitch) {
-        this.crosshairswitch = crosshairswitch;
+    public void setCrosshairSwitch(boolean value) {
+        setBoolean(Key.CrosshairSwitch, value);
     }
 
-    public void setCrosshair(boolean crosshair) {
-        this.crosshair = crosshair;
+    public void setCrosshair(boolean value) {
+        setBoolean(Key.Crosshair, value);
     }
 
-    public void setCombattext(boolean combattext) {
-        this.combattext = combattext;
+    public void setCombattext(boolean value) {
+        setBoolean(Key.CombatText, value);
     }
 
-    public void setAnnouncer(boolean announcer) {
-        this.announcer = announcer;
+    public void setAnnouncer(boolean value) {
+        setBoolean(Key.Announcer, value);
     }
 
-    public void setDomination(boolean domination) {
-        this.domination = domination;
+    public void setDomination(boolean value) {
+        setBoolean(Key.Domination, value);
     }
 
-    public void setHitsounds(boolean hitsounds) {
-        this.hitsounds = hitsounds;
+    public void setHitsounds(boolean value) {
+        setBoolean(Key.Hitsounds, value);
     }
 
-    public void setVoice(boolean voice) {
-        this.voice = voice;
+    public void setVoice(boolean value) {
+        setBoolean(Key.Voice, value);
     }
 
-    public void setSteamCloud(boolean steamcloud) {
-        this.steamcloud = steamcloud;
+    public void setSteamCloud(boolean value) {
+        setBoolean(Key.SteamCloud, value);
     }
 
-    public void setDxlevel(int dxlevel) {
-        this.dxlevel = dxlevel;
+    public void setDxlevel(int value) {
+        setInt(Key.DxLevel, value);
     }
 
     public int getHeight() {
-        return height;
+        return getInt(Key.Height);
     }
 
     public int getWidth() {
-        return width;
+        return getInt(Key.Width);
     }
 
     public int getFramerate() {
-        return framerate;
+        return getInt(Key.Framerate);
     }
 
     public String getHud() {
-        return hud;
+        return getString(Key.Hud);
     }
 
     public int getViewmodelFov() {
-        return viewmodelfov;
+        return getInt(Key.ViewmodelFov);
     }
 
     public boolean getMotionBlur() {
-        return motionblur;
+        return getBoolean(Key.MotionBlur);
     }
 
     public String getViewmodelSwitch() {
-        return viewmodelswitch;
+        return getString(Key.ViewmodelSwitch);
     }
 
     public boolean getCrosshairSwitch() {
-        return crosshairswitch;
+        return getBoolean(Key.CrosshairSwitch);
     }
 
     public boolean getCrosshair() {
-        return crosshair;
+        return getBoolean(Key.Crosshair);
     }
 
     public boolean getCombattext() {
-        return combattext;
+        return getBoolean(Key.CombatText);
     }
 
     public boolean getAnnouncer() {
-        return announcer;
+        return getBoolean(Key.Announcer);
     }
 
     public boolean getDomination() {
-        return domination;
+        return getBoolean(Key.Domination);
     }
 
     public boolean getHitsounds() {
-        return hitsounds;
+        return getBoolean(Key.Hitsounds);
     }
 
     public boolean getVoice() {
-        return voice;
+        return getBoolean(Key.Voice);
     }
 
     public boolean getSteamCloud() {
-        return steamcloud;
+        return getBoolean(Key.SteamCloud);
     }
 
     public int getDxlevel() {
-        return dxlevel;
+        return getInt(Key.DxLevel);
     }
 }
