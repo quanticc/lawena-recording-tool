1333ea7e0ad340b70714bab3effadb3589925822
Ivan Abarca <ivan.abarca@usach.cl>
2015-01-03 05:29:16 -0300

    changed some cfg files and how they are loaded

--- src/main/java/com/github/lawena/app/model/ConfigWriterGo.java
+++ src/main/java/com/github/lawena/app/model/ConfigWriterGo.java
@@ -3,14 +3,9 @@ package com.github.lawena.app.model;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.github.lawena.profile.Key;
 
 public class ConfigWriterGo extends ConfigWriter {
-
-  private static final Logger log = LoggerFactory.getLogger(ConfigWriterGo.class);
 
   public ConfigWriterGo(Settings settings) {
     super(settings);
@@ -21,24 +16,19 @@ public class ConfigWriterGo extends ConfigWriter {
     // in CS:GO you can't alias existing convars or concommands
     List<String> lines = new ArrayList<>();
     addFramerateLines(lines);
+    // convars tied to value from config key
     lines.add("viewmodel_fov " + Key.viewmodelFov.getValue(settings));
     lines.add("crosshair " + (!Key.noCrosshair.getValue(settings) ? "1" : "0"));
     lines.add("voice_enable " + (!Key.noVoice.getValue(settings) ? "1" : "0"));
     lines.add("voice_caster_enable " + (!Key.noCasterVoice.getValue(settings) ? "1" : "0"));
-    lines.add("net_graph 0");
-    lines.add("cl_showfps 0");
-    lines.add("cl_draw_only_deathnotices 1");
-    lines.add("volume 0.2");
-    lines.add("hidehud 8329");
-    lines.add("hidehud 8320");
-    lines.add("hud_takesshots 0");
-    lines.add("");
-    lines.add("engine_no_focus_sleep 0");
-    lines.add(Key.extConVars.getValue(settings));
     lines.add((Key.viewmodelSwitch.getValue(settings).equals("off") ? "//" : "")
         + "lockviewmodelson");
     lines.add((Key.viewmodelSwitch.getValue(settings).equals("on") ? "//" : "")
         + "lockviewmodelsoff");
+    // values from cfg
+    lines.add("echo \"[Lawena] Applying user custom settings\"");
+    lines.add("exec static.cfg");
+    lines.add(Key.extConVars.getValue(settings));
     return lines;
   }
 
--- src/main/java/com/github/lawena/app/model/ConfigWriterTf.java
+++ src/main/java/com/github/lawena/app/model/ConfigWriterTf.java
@@ -3,14 +3,9 @@ package com.github.lawena.app.model;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.github.lawena.profile.Key;
 
 public class ConfigWriterTf extends ConfigWriter {
-
-  private static final Logger log = LoggerFactory.getLogger(ConfigWriterTf.class);
 
   public ConfigWriterTf(Settings settings) {
     super(settings);
@@ -19,11 +14,12 @@ public class ConfigWriterTf extends ConfigWriter {
   @Override
   protected List<String> writeGameConfig() {
     List<String> lines = new ArrayList<>();
+    addNotice(lines);
     addFramerateLines(lines);
+    lines.add("cl_cloud_settings 0");
     lines.add("mat_motion_blur_enabled " + (Key.motionBlur.getValue(settings) ? "1" : "0"));
     lines.add("mat_motion_blur_forward_enabled " + (Key.motionBlur.getValue(settings) ? "1" : "0"));
     lines.add("mat_motion_blur_strength " + (Key.motionBlur.getValue(settings) ? "1" : "0"));
-    lines.add("cl_cloud_settings 0");
     lines.add("viewmodel_fov_demo " + Key.viewmodelFov.getValue(settings));
     boolean crosshairswitch = !Key.noCrosshairSwitch.getValue(settings);
     lines.add((crosshairswitch ? "//" : "") + "cl_crosshair_file \"\"");
@@ -37,46 +33,18 @@ public class ConfigWriterTf extends ConfigWriter {
     lines.add("hud_combattext_healing " + (!Key.noDamageNumbers.getValue(settings) ? "1" : "0"));
     lines.add("tf_dingalingaling " + (!Key.noHitsounds.getValue(settings) ? "1" : "0"));
     lines.add("voice_enable " + (!Key.noVoice.getValue(settings) ? "1" : "0"));
-    lines.add("alias voice_enable \"\"");
-    lines.add("cl_autorezoom 0");
-    lines.add("hud_saytext_time 0");
-    lines.add("net_graph 0");
-    lines.add("alias net_graph \"\"");
-    lines.add("alias voice_menu_1 \"\"");
-    lines.add("alias voice_menu_2 \"\"");
-    lines.add("alias voice_menu_3 \"\"");
-    lines.add("cl_showfps 0");
-    lines.add("alias cl_showfps \"\"");
-    lines.add("volume 0.2");
-    lines.add("hud_fastswitch 1");
     lines.add("cl_hud_minmode " + (Key.hudMinmode.getValue(settings) ? "1" : "0"));
-    lines.add("cl_hud_playerclass_playermodel_showed_confirm_dialog 1");
     lines.add("cl_hud_playerclass_use_playermodel "
         + (Key.hudPlayerModel.getValue(settings) ? "1" : "0"));
-    lines.add("tf_training_has_prompted_for_loadout 1");
-    lines.add("tf_training_has_prompted_for_training 1");
-    lines.add("tf_training_has_prompted_for_offline_practice 1");
-    lines.add("tf_training_has_prompted_for_forums 1");
-    lines.add("tf_training_has_prompted_for_options 1");
-    lines.add("tf_training_has_prompted_for_loadout 1");
-    lines.add("tf_explanations_charinfo_armory_panel 1");
-    lines.add("tf_explanations_charinfopanel 1");
-    lines.add("tf_show_preset_explanation_in_class_loadout 0");
-    lines.add("tf_show_taunt_explanation_in_class_loadout 0");
-    lines.add("tf_explanations_craftingpanel 1");
-    lines.add("tf_explanations_discardpanel 1");
-    lines.add("tf_explanations_store 1");
-    lines.add("tf_explanations_backpackpanel 1");
-    lines.add("engine_no_focus_sleep 0");
-    lines.add("cl_spec_carrieditems 0");
-    lines.add("tf_hud_target_id_disable_floating_health 1");
-    lines.add(Key.extConVars.getValue(settings));
     lines.add((Key.viewmodelSwitch.getValue(settings).equals("off") ? "//" : "")
         + "lockviewmodelson");
     lines.add((Key.viewmodelSwitch.getValue(settings).equals("on") ? "//" : "")
         + "lockviewmodelsoff");
     lines.add((crosshairswitch ? "//" : "") + "lockcrosshair");
     lines.add((crosshairswitch ? "//" : "") + "alias toggle \"\"");
+    lines.add("echo \"[Lawena] Applying user custom settings\"");
+    lines.add("exec static.cfg");
+    lines.add(Key.extConVars.getValue(settings));
     return lines;
   }
 
