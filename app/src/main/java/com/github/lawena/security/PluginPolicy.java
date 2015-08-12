3ae582107100ab4555cec991ea20ddce7cf3a153
Ivan Abarca <ivan.abarca@usach.cl>
2015-08-12 13:05:02 -0300

    Restricted some plugin permissions

--- app/src/main/java/com/github/lawena/security/PluginPolicy.java
+++ app/src/main/java/com/github/lawena/security/PluginPolicy.java
@@ -1,6 +1,7 @@
 package com.github.lawena.security;
 
 import java.io.FilePermission;
+import java.net.SocketPermission;
 import java.nio.file.Paths;
 import java.security.AllPermission;
 import java.security.Permission;
@@ -40,12 +41,38 @@ public class PluginPolicy extends Policy {
 
     private PermissionCollection pluginPermissions() {
         Permissions p = new Permissions();
+        // low risk permissions
         p.add(new FilePermission("<<ALL FILES>>", "read"));
         p.add(new FilePermission(Paths.get("").toAbsolutePath().getParent() + "/-", "read, write, execute, delete, readlink"));
-        p.add(new PropertyPermission("*", "read"));
-        p.add(new RuntimePermission("accessClassInPackage.*"));
         p.add(new RuntimePermission("getClassLoader"));
         p.add(new RuntimePermission("preferences"));
+        p.add(new PropertyPermission("user.dir", "read"));
+
+        // for debugging
+//        p.add(new PropertyPermission("*", "read"));
+//        p.add(new RuntimePermission("accessClassInPackage.*"));
+
+        // default permissions
+        p.add(new SocketPermission("localhost:0", "listen"));
+        p.add(new PropertyPermission("java.version", "read"));
+        p.add(new PropertyPermission("java.vendor", "read"));
+        p.add(new PropertyPermission("java.vendor.url", "read"));
+        p.add(new PropertyPermission("java.class.version", "read"));
+        p.add(new PropertyPermission("os.name", "read"));
+        p.add(new PropertyPermission("os.version", "read"));
+        p.add(new PropertyPermission("os.arch", "read"));
+        p.add(new PropertyPermission("file.separator", "read"));
+        p.add(new PropertyPermission("path.separator", "read"));
+        p.add(new PropertyPermission("line.separator", "read"));
+        p.add(new PropertyPermission("java.specification.version", "read"));
+        p.add(new PropertyPermission("java.specification.vendor", "read"));
+        p.add(new PropertyPermission("java.specification.name", "read"));
+        p.add(new PropertyPermission("java.vm.specification.version", "read"));
+        p.add(new PropertyPermission("java.vm.specification.vendor", "read"));
+        p.add(new PropertyPermission("java.vm.specification.name", "read"));
+        p.add(new PropertyPermission("java.vm.version", "read"));
+        p.add(new PropertyPermission("java.vm.vendor", "read"));
+        p.add(new PropertyPermission("java.vm.name", "read"));
         return p;
     }
 
